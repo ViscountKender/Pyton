22cc7be9c931a1fa6267126b80b7683ddc2fc445
Denis <Siyanov.d@sar.garm.ru>
2022-03-12 22:20:00 +0400

    task12

--- task_12.py
+++ task_12.py
@@ -1,4 +1,7 @@
 # При заданном целом числе n посчитайте n + nn + nnn.
-number = int(input("Введите число: "))
-result = number + number**2 + number**3
-print(result)
+number = (input("Введите число: "))
+num1 = number + number
+num2 = number + number + number
+sum = int(number)+int(num1) + int(num2)
+print(sum)
+
